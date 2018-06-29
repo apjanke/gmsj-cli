ae7168c34a7990b75b87611867fa9f5a44e6e5f7
Andrew Janke <andrew@apjanke.net>
2018-06-29 06:01:37 -0400

    Add valid subcommand list to invalid-subcommand error output

--- src/main/java/net/apjanke/gmsjcli/MainApp.java
+++ src/main/java/net/apjanke/gmsjcli/MainApp.java
@@ -4,6 +4,8 @@ import net.apjanke.gmsjcli.app.GeoFuzzCmd;
 import net.apjanke.gmsjcli.app.GeocodeCmd;
 import net.apjanke.gmsjcli.app.Subcommand;
 
+import java.util.Arrays;
+
 public class MainApp {
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -19,6 +21,7 @@ public class MainApp {
         String[] remainingArgs = new String[args.length - 1];
         System.arraycopy(args, 1, remainingArgs, 0, args.length - 1);
 
+        String[] validSubCmds = { "geocode", "geofuzz" };
         Subcommand cmd = null;
         if ("geocode".equals(subcommand)) {
             cmd = new GeocodeCmd();
@@ -26,6 +29,7 @@ public class MainApp {
             cmd = new GeoFuzzCmd();
         } else {
             System.err.println("Error: Unrecognized subcommand: " + subcommand);
+            System.err.println("Error: Valid subcommands are: " + Arrays.toString(validSubCmds));
             System.exit(1);
         }
         int status = cmd.run(remainingArgs);
